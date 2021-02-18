1d33119b8c71d5a9b5762158652814f016d34778
Lucas <luca.coutinho1@gmail.com>
2021-02-18 08:21:45 -0300

    refactoring test

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,34 +22,13 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        $randomNumber = $this->faker->boolean(50);
-        $randomType =  $randomNumber ? 'usuario' : 'lojista';
-
-        if ($randomType === 'lojista') {
-            $cnpj = intval($this->faker->unique()->randomNumber(7) . $this->faker->unique()->randomNumber(7));
-        }
-        if ($randomNumber === true) {
-            return [
-                'nome' => $this->faker->firstName(),
-                'cpf' => intval($this->faker->unique()->randomNumber(5) . $this->faker->unique()->randomNumber(6)),
-                'cnpj' => NULL,
-                'tipo' => $randomType,
-                'email' => $this->faker->unique()->safeEmail,
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
-            ];
-        } else if ($randomNumber === false) {
-            return [
-                'nome' => $this->faker->firstName(),
-                'cpf' => intval($this->faker->unique()->randomNumber(5) . $this->faker->unique()->randomNumber(6)),
-                'cnpj' => $cnpj,
-                'tipo' => $randomType,
-                'email' => $this->faker->unique()->safeEmail,
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
-            ];
-        }
+        return [
+            'nome' => $this->faker->firstName(),
+            'cpf' => intval($this->faker->unique()->randomNumber(5) . $this->faker->unique()->randomNumber(6)),
+            'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ];
     }
 }
--- tests/Feature/TransactionTest.php
+++ tests/Feature/TransactionTest.php
@@ -99,41 +99,69 @@ class TransactionTest extends TestCase
     //     }
     // }
 
-    public function test_transaction_negative_balance()
+    // public function test_transaction_negative_balance()
+    // {
+
+    //     $payeeBalance =
+    //         $payee = User::create([
+    //             'nome' => $this->faker->firstName() . $this->faker->firstName(),
+    //             'cpf' => intval($this->faker->unique()->randomNumber(5) . $this->faker->unique()->randomNumber(6)),
+    //             'cnpj' => intval($this->faker->unique()->randomNumber(7) . $this->faker->unique()->randomNumber(7)),
+    //             'tipo' => 'lojista',
+    //             'email' => $this->faker->unique()->safeEmail,
+    //             'email_verified_at' => now(),
+    //             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+    //             'remember_token' => Str::random(10),
+    //         ]);
+
+    //     $payee->balance()->create(['balance' => $this->faker->randomNumber(3)]);
+
+    //     $payer = User::create([
+    //         'nome' => $this->faker->firstName(),
+    //         'cpf' => intval($this->faker->unique()->randomNumber(5) . $this->faker->unique()->randomNumber(6)),
+    //         'cnpj' => NULL,
+    //         'tipo' => 'usuario',
+    //         'email' => $this->faker->unique()->safeEmail,
+    //         'email_verified_at' => now(),
+    //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+    //         'remember_token' => Str::random(10),
+    //     ]);
+
+    //     $payer->balance()->create(['balance' => $this->faker->randomNumber(3)]);
+
+    //     $balance = Balance::where('user_id', $payee->id)->first()->balance;
+
+    //     $value = $this->faker->randomNumber(6);
+
+    //     if ($value > $balance) {
+    //         $response = $this->postJson('api/transfer', [
+    //             'payer' => $payer->id,
+    //             'payee' => $payee->id,
+    //             'value' => $value,
+    //         ])->assertStatus(406)->assertExactJson(['error' => 'Creditos insuficientes.']);
+    //     }
+    // }
+
+    public function test_transaction_negative_balance2()
     {
-        $payee = User::create([
-            'nome' => $this->faker->firstName() . $this->faker->firstName(),
-            'cpf' => intval($this->faker->unique()->randomNumber(5) . $this->faker->unique()->randomNumber(6)),
-            'cnpj' => intval($this->faker->unique()->randomNumber(7) . $this->faker->unique()->randomNumber(7)),
-            'tipo' => 'lojista',
-            'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ]);
 
-        $payee->balance()->create(['balance' => $this->faker->randomNumber(3)]);
-
-        $payer = User::create([
-            'nome' => $this->faker->firstName(),
-            'cpf' => intval($this->faker->unique()->randomNumber(5) . $this->faker->unique()->randomNumber(6)),
-            'cnpj' => NULL,
+        $payer = User::factory(4)->create([
             'tipo' => 'usuario',
-            'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'cnpj' => NULL,
         ]);
 
-        $payer->balance()->create(['balance' => $this->faker->randomNumber(3)]);
+        $payee = User::factory()->create([
+            'tipo' => 'lojista',
+            'cnpj' => intval($this->faker->unique(true, 3)->randomNumber(7) . $this->faker->unique(true, 4)->randomNumber(7)),
+        ]);
 
-        $balance = Balance::where('user_id', $payee->id)->first()->balance;
+        $balance = Balance::factory()->create(['user_id' => $this->faker->randomElement($payer->pluck('id')->toArray())]);
 
         $value = $this->faker->randomNumber(6);
 
-        if ($value > $balance) {
+        if ($value > $balance->balance) {
             $response = $this->postJson('api/transfer', [
-                'payer' => $payer->id,
+                'payer' => $balance->user_id,
                 'payee' => $payee->id,
                 'value' => $value,
             ])->assertStatus(406)->assertExactJson(['error' => 'Creditos insuficientes.']);
